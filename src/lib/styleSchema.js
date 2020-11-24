6d1620424e4def510abee22778744524016984d3
pkkwilliam <pkkwilliam919@gmail.com>
2020-11-24 23:20:55 +0800

    update styleschema

--- src/lib/styleSchema.js
+++ src/lib/styleSchema.js
@@ -2,12 +2,14 @@ export const styleSchema = {
   color: {
     background: "#f6f6f6",
     black: "#000000",
-    primaryDark: "#0066ff",
-    primaryLight: "#B5DBEB",
+    primaryDark: "#FF914D",
+    primaryLight: "#FFA46C",
     secondaryDark: "#7D7D7D",
     secondaryLight: "#f6f6f6",
     white: "#FFFFFF",
   },
   shadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  shadowLight:
+    "0 0px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.0)",
   text: {},
 };
