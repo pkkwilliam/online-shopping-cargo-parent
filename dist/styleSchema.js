7ec5df83a291699a81032b330958373e4808fee5
pkkwilliam <pkkwilliam919@gmail.com>
2021-01-12 22:14:03 +0800

    build

--- dist/styleSchema.js
+++ dist/styleSchema.js
@@ -3,10 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+var PRIMARY_DARK = "rgba(252,91,1,1)";
+var PRIMARY_LIGHT = "rgba(252,150,3,1)";
+
 var styleSchema = exports.styleSchema = {
   color: {
     background: "#f6f6f6",
     black: "#000000",
+    primaryGradient: "linear-gradient(90deg, " + PRIMARY_LIGHT + ") 0%, " + PRIMARY_DARK + " 100%)",
     primaryDark: "#FF914D",
     primaryLight: "#FFA46C",
     secondaryDark: "#7D7D7D",
