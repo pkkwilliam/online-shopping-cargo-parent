c08a53fdfa47ee1e9a2d8f1b1aacbe969cd9ad52
pkkwilliam <pkkwilliam919@gmail.com>
2021-03-31 21:06:04 +0800

    revert

--- dist/applicationComponent.view.js
+++ dist/applicationComponent.view.js
@@ -6,13 +6,17 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-exports.default = ApplicationComponentView;
-
 var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var ApplicationModal = _react2.default.lazy(function () {
   return import("./applicationModal");
@@ -20,38 +24,55 @@ var ApplicationModal = _react2.default.lazy(function () {
 var ApplicationToast = _react2.default.lazy(function () {
   return import("./applicationToast");
 });
-// export default class ApplicationComponentView extends Component {
-//   Wrapper = ({ children }) => {
-//     const { modal, onCloseModal, onCloseToast, toast } = this.props;
-//   };
-// }
 
-function Modal(_ref) {
-  var modal = _ref.modal,
-      onCloseModal = _ref.onCloseModal;
+var ApplicationComponentView = function (_Component) {
+  _inherits(ApplicationComponentView, _Component);
+
+  function ApplicationComponentView() {
+    var _ref;
+
+    var _temp, _this, _ret;
+
+    _classCallCheck(this, ApplicationComponentView);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = ApplicationComponentView.__proto__ || Object.getPrototypeOf(ApplicationComponentView)).call.apply(_ref, [this].concat(args))), _this), _this.Wrapper = function (_ref2) {
+      var children = _ref2.children;
+      var _this$props = _this.props,
+          modal = _this$props.modal,
+          onCloseModal = _this$props.onCloseModal,
+          onCloseToast = _this$props.onCloseToast,
+          toast = _this$props.toast;
+
+      return _react2.default.createElement(
+        _react.Fragment,
+        null,
+        _react2.default.createElement(Modal, { onCloseModal: onCloseModal, modal: modal }),
+        _react2.default.createElement(Toast, { onCloseToast: onCloseToast, toast: toast }),
+        children
+      );
+    }, _temp), _possibleConstructorReturn(_this, _ret);
+  }
+
+  return ApplicationComponentView;
+}(_react.Component);
+
+exports.default = ApplicationComponentView;
+
+
+function Modal(_ref3) {
+  var modal = _ref3.modal,
+      onCloseModal = _ref3.onCloseModal;
 
   return _react2.default.createElement(ApplicationModal, _extends({ onClose: onCloseModal }, modal));
 }
 
-function Toast(_ref2) {
-  var toast = _ref2.toast,
-      onCloseToast = _ref2.onCloseToast;
+function Toast(_ref4) {
+  var toast = _ref4.toast,
+      onCloseToast = _ref4.onCloseToast;
 
   return _react2.default.createElement(ApplicationToast, _extends({ onClose: onCloseToast }, toast));
-}
-
-function ApplicationComponentView(props) {
-  var children = props.children,
-      modal = props.modal,
-      onCloseModal = props.onCloseModal,
-      onCloseToast = props.onCloseToast,
-      toast = props.toast;
-
-  return _react2.default.createElement(
-    _react.Fragment,
-    null,
-    _react2.default.createElement(Modal, { onCloseModal: onCloseModal, modal: modal }),
-    _react2.default.createElement(Toast, { onCloseToast: onCloseToast, toast: toast }),
-    children
-  );
 }
--- src/lib/applicationComponent.view.js
+++ src/lib/applicationComponent.view.js
@@ -2,11 +2,18 @@ import React, { Component, Fragment } from "react";
 
 const ApplicationModal = React.lazy(() => import("./applicationModal"));
 const ApplicationToast = React.lazy(() => import("./applicationToast"));
-// export default class ApplicationComponentView extends Component {
-//   Wrapper = ({ children }) => {
-//     const { modal, onCloseModal, onCloseToast, toast } = this.props;
-//   };
-// }
+export default class ApplicationComponentView extends Component {
+  Wrapper = ({ children }) => {
+    const { modal, onCloseModal, onCloseToast, toast } = this.props;
+    return (
+      <Fragment>
+        <Modal onCloseModal={onCloseModal} modal={modal} />
+        <Toast onCloseToast={onCloseToast} toast={toast} />
+        {children}
+      </Fragment>
+    );
+  };
+}
 
 function Modal({ modal, onCloseModal }) {
   return <ApplicationModal onClose={onCloseModal} {...modal} />;
@@ -14,15 +21,4 @@ function Modal({ modal, onCloseModal }) {
 
 function Toast({ toast, onCloseToast }) {
   return <ApplicationToast onClose={onCloseToast} {...toast} />;
-}
-
-export default function ApplicationComponentView(props) {
-  const { children, modal, onCloseModal, onCloseToast, toast } = props;
-  return (
-    <Fragment>
-      <Modal onCloseModal={onCloseModal} modal={modal} />
-      <Toast onCloseToast={onCloseToast} toast={toast} />
-      {children}
-    </Fragment>
-  );
 }
